f87b0d643a65ee013ff717775f9977a18970b712
Leo Mikko Uson <lancerthortan@gmail.com>
2023-08-15 16:13:02 +0800

    Added update task

--- backend/src/routes/taskRouter.ts
+++ backend/src/routes/taskRouter.ts
@@ -1,8 +1,17 @@
 import express from "express";
 import protect from "../middlewares/authMiddleware";
-import { createTask } from "../controllers/taskController";
+import {
+  createTask,
+  deleteTask,
+  getSpecificTask,
+  getTask,
+  updateTask,
+} from "../controllers/taskController";
 const TaskRouter = express.Router();
 
 TaskRouter.post("/", protect, createTask);
-
+TaskRouter.get("/", protect, getTask);
+TaskRouter.delete("/:id", protect, deleteTask);
+TaskRouter.put("/:id", protect, updateTask);
+TaskRouter.get("/:id", protect, getSpecificTask);
 export default TaskRouter;
