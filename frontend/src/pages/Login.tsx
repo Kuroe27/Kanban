24203c2756e9ecbd20d75611fe851a0a26544b98
Leo Mikko Uson <lancerthortan@gmail.com>
2023-08-21 23:04:02 +0800

    Update Login style

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,31 +1,31 @@
+import React from "react";
+
 const Login = () => {
   return (
-    <div className="container h-[100vh] w-full flex justify-center items-center text-center ">
-      <form className=" h-1/2 w-1/2 px-10 ">
+    <form className=" min-h-[100vh] text-gray-300 ">
+      <div className="flex flex-col justify-center items-center min-h-[100vh]">
         <h1 className="text-3xl text-gray-50 my-5  font-semibold ">
           Login to KanbanFlow
         </h1>
 
-        <div className="flex flex-col ">
-          <input
-            type="text"
-            className="w-full bg-gray-200 border-2 border-gray-700 text-2xl p-1 rounded-md mb-2"
-            placeholder="Email"
-          />
-          <input
-            type="text"
-            className="w-full bg-gray-200 border-2 border-gray-700 text-2xl p-1 rounded-md  mb-4"
-            placeholder="Password"
-          />
-          <button className=" bg-gray-100 text-lg w-full p-1 rounded-lg mb-2">
-            Login
-          </button>
-          <button className=" bg-gray-100 text-lg w-full p-1 rounded-lg mb-2 ">
-            Sign up
-          </button>
-        </div>
-      </form>
-    </div>
+        <input
+          type="text"
+          className=" bg-gray-800 border-2 border-gray-200 text-1xl p-2 rounded-md mb-2 w-96 placeholder-text-xl"
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          className=" bg-gray-800 border-2 border-gray-200 text-2xl p-1 rounded-md mb-2 w-96 placeholder-text-xl"
+          placeholder="Password"
+        />
+        <button className=" bg-gray-100 text-lg w-96 p-2 rounded-lg mb-2 text-gray-900">
+          Login
+        </button>
+        <button className=" bg-gray-100 text-lg  w-96 p-2 rounded-lg mb-2  text-gray-900">
+          Sign up
+        </button>
+      </div>
+    </form>
   );
 };
 
