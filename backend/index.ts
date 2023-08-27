de01c0571f0c218dcc3f0f625b6fcf8ab7d0ddf7
Leo Mikko Uson <lancerthortan@gmail.com>
2023-08-27 19:02:19 +0800

    Update domain

--- backend/index.ts
+++ backend/index.ts
@@ -27,19 +27,6 @@ const allowedOrigins = [
   "https://kanbanflow-nine.vercel.app",
 ];
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", allowedOrigins);
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,PUT,POST,DELETE,UPDATE,OPTIONS"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-  );
-  next();
-});
 app.use(
   cors({
     origin: allowedOrigins,
